71399ea8f125fa64ca27c0534dd8a42fc20cba5a
Adedamola <adedamola.adenuga04@gmail.com>
2024-11-11 14:08:45 +0100

    day 1

---
+++ day1_intro.py
@@ -0,0 +1,10 @@
+# Program to print my introduction
+
+print("Hello World")
+print("---------------------------------")
+print("My name is Adedamola Aliyah")
+print("I am a systems engineering student and I have a great interest in Artificial Intelligence, Machine Learning and Robotics")
+print("I hope to use this challenge to revise and improve my python skills")
+print("Stick with me and I hope I stay with this challenge till the end")
+print("Thank you")
+print("---------------------------------")
