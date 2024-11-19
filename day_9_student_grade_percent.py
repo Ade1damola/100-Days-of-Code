217c6b98046e54b3727e417813167ac6bfa2de10
Ade1damola <adedamola.adenuga04@gmail.com>
2024-11-19 17:31:31 +0100

    day 9

---
+++ day_9_student_grade_percent.py
@@ -0,0 +1,14 @@
+score = float(input("Enter your score over 100: "))
+
+if score >= 70:
+    print("Grade: A")
+elif score >= 60:
+    print("Grade: B")
+elif score >= 50:
+    print("Grade: C")
+elif score >= 40:
+    print("Grade: D")
+elif score >= 30:
+    print("Grade: E")
+else:
+    print("Grade: F")
